7332b1bb7fd17a1692440e67b5b4ce9818747208
Gustavo LIBOTTE <gustavolibotte@gmail.com>
2023-05-13 15:32:02 -0300

    + A class to calculate and save the results of a particular case study.

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -1,25 +1,31 @@
+import argparse
+
 from cases.study_cases import StudyCases
+from utils.array_file_manager import ArrayFileManager
 
-def main():
-    # # Saving dataset to file
-    # directory = "data/"
-    # filename = f"training_occupancy_{occupancy}.csv"
-    # ArrayFileManager.save_array_to_file(directory, filename, dataset)
+def main(data_path, occupancy):
+    # Defining the number of slices and slice size (TODO: change)
+    n_slices = 1_000_000
+    slice_size = 7
 
-    # Reading dataset from file
-    # directory = "data/"
-    # filename = f"training_occupancy_{occupancy}.csv"
-    # dataset = ArrayFileManager.read_array_from_file(directory, filename)
+    filename = f"training_occupancy_{occupancy}_slice_{slice_size}.csv"
+    training_dataset = ArrayFileManager.read_array_from_file(data_path, filename)
 
-    # Setting up the pulse shape path
-    pulse_shape_path = 'analysis/base/unipolar-pulse-shape.dat'
+    filename = f"test_occupancy_{occupancy}_slice_{slice_size}.csv"
+    test_dataset = ArrayFileManager.read_array_from_file(data_path, filename)
 
-    analysis_cases = StudyCases(pulse_shape_path)
-    analysis_cases.case_1()
+    # Running results
+    analysis_cases = StudyCases(training_dataset, test_dataset, n_slices, slice_size, occupancy)
+    analysis_cases.run_case_1(save_file=True)
 
 if __name__ == '__main__':
-    try:
-        main()
-    except FileNotFoundError as e:
-        print(f'Error: {e}')
-        exit(1)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--data_path", type=str, help="Calorimetry pulse data relative path (default: data/)")
+    parser.add_argument("--occupancy", type=float, help="Occupancy (0.1 | 0.3 | 0.5)")
+    
+    args = parser.parse_args()
+    if args.data_path and args.occupancy:
+        main(args.data_path, args.occupancy)
+    else:
+        print("Please specify both the calorimetry pulse data relative path and occupancy.")
--- analysis/cases/study_cases.py
+++ analysis/cases/study_cases.py
@@ -1,32 +1,59 @@
-import os
-from pycps import Random, TextFilePulseShape
-
-from datasets.setup_dataset import SetupDataset
+import numpy as np
 from filters.least_squares import LS
+from statistics.analysis_statistics import AnalysisStatistics
+from utils.array_file_manager import ArrayFileManager
+from utils.plot_manager import GraphPlotter
 
 class StudyCases:
+    """
+    A class to run and analyze study cases on filtered datasets.
 
-    def __init__(self, pulse_shape_path):
-        # Reading the pulse shape file
-        abs_path = os.path.abspath(os.path.join(os.getcwd(), pulse_shape_path))
-        if os.path.isfile(abs_path):
-            self.pulse_shape = TextFilePulseShape(abs_path)
-        else:
-            raise FileNotFoundError(f'File {pulse_shape_path} was not found.')
+    Parameters
+    ----------
+    training_dataset : numpy.ndarray
+        The training dataset containing time, samples, and amplitudes.
+    test_dataset : numpy.ndarray
+        The test dataset containing time, samples, and amplitudes.
+    n_slices : int
+        The number of slices in the datasets.
+    slice_size : int
+        The size of each slice in the datasets.
+    occupancy : float
+        The occupancy of the dataset.
+
+    Methods
+    -------
+    run_case_1(save_file: bool)
+        Runs study case 1 by estimating amplitudes, computing errors, and plotting results.
+    __handle_dataset(dataset: numpy.ndarray)
+        Handles the input dataset by reshaping the time, samples, and amplitudes arrays.
+    __run_filtering(samples: numpy.ndarray, amplitudes: numpy.ndarray)
+        Runs the least squares filter on the input samples and amplitudes.
+    """
     
-    def case_1(self):
-        # Setting up the random seed
-        Random.seed(0)
+    def __init__(self, training_dataset, test_dataset, n_slices, slice_size, occupancy) -> None:
+        self.training_dataset = training_dataset
+        self.test_dataset = test_dataset
+        self.n_slices = n_slices
+        self.slice_size = slice_size
+        self.occupancy = occupancy
+    
+    def run_case_1(self, save_file):
+        """
+        Runs study case 1 by estimating amplitudes, computing errors, and plotting results.
+        
+        Parameters
+        ----------
+        save_file : bool
+            Whether to save the error amplitudes to a file.
+        
+        Returns
+        -------
+        None
+        """
 
-        # Defining the number of slices and slice size
-        self.n_slices = 100
-        self.slice_size = 7
-
-        # Defining the occupancy
-        self.occupancy = 0.1
-
-        # Creating the dataset
-        _, training_samples, training_amplitudes = self.__create_dataset()
+        # Organizing loaded data
+        _, training_samples, training_amplitudes = self.__handle_dataset(self.training_dataset)
 
         # Taking the central amplitudes of each window
         training_amplitudes = training_amplitudes[:, self.slice_size//2]
@@ -34,19 +61,65 @@ class StudyCases:
         # Running least squares
         weights, success = self.__run_filtering(training_samples, training_amplitudes)
         if success:
-            print(f"weights = {weights}")
+            estimated_amplitudes = AnalysisStatistics.estimate_amplitudes(training_samples, weights,
+                                                                         self.n_slices, self.slice_size)
+
+            # Calculating the error between test dataset and estimated amplitudes
+            _, _, test_amplitudes = self.__handle_dataset(self.test_dataset)
+            error_amplitudes = AnalysisStatistics.compare_amplitudes(test_amplitudes, self.n_slices,
+                                                                     self.slice_size, estimated_amplitudes)
+            
+            # Computing sample mean and standard deviation
+            error_mean = np.mean(error_amplitudes)
+            error_stdev = np.std(error_amplitudes)
+            
+            # Plotting results
+            plotter = GraphPlotter(figsize=(5, 3))
+            plotter.hist_plot(error_amplitudes, error_mean, error_stdev, "Error")
+
+            if save_file:
+                ArrayFileManager.save_array_to_file("results/",
+                                                    f"error_occupancy_{self.occupancy}_slice_{self.slice_size}.csv",
+                                                    error_amplitudes)
+        else:
+            print("Least squares could not find a feasible solution.")
     
-    def __create_dataset(self):
-        dataset_obj = SetupDataset(self.pulse_shape, self.n_slices, self.slice_size)
-        dataset_obj.create_sliced_dataset(self.occupancy)
+    def __handle_dataset(self, dataset):
+        """
+        Handles the input dataset by reshaping the time, samples, and amplitudes arrays.
         
-        times = dataset_obj.get_dataset_times()
-        samples = dataset_obj.get_dataset_samples()
-        amplitudes = dataset_obj.get_dataset_amplitudes()
+        Parameters
+        ----------
+        dataset : numpy.ndarray
+            The dataset containing time, samples, and amplitudes.
+        
+        Returns
+        -------
+        Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]
+            A tuple of times, samples, and amplitudes arrays.
+        """
+        times = np.reshape(dataset[:, 0], (self.n_slices, self.slice_size))
+        samples = np.reshape(dataset[:, 1], (self.n_slices, self.slice_size))
+        amplitudes = np.reshape(dataset[:, 2], (self.n_slices, self.slice_size))
 
         return (times, samples, amplitudes)
     
     def __run_filtering(self, samples, amplitudes):
+        """
+        Runs the least squares filter on the input samples and amplitudes.
+        
+        Parameters
+        ----------
+        samples : numpy.ndarray
+            The samples array.
+        amplitudes : numpy.ndarray
+            The amplitudes array.
+        
+        Returns
+        -------
+        Tuple[numpy.ndarray, bool]
+            A tuple of weights and success status.
+        """
         n_filter = self.slice_size
         
         ls = LS(samples, amplitudes, n_filter)
