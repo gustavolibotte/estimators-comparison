0d5eb03920dd8aed4d0e93936186ac79779f23b6
Gustavo LIBOTTE <gustavolibotte@gmail.com>
2023-05-16 21:13:09 -0300

    Changes to allow other amounts of weights to be considered

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -1,31 +1,46 @@
 import argparse
 
+import numpy as np
+
 from cases.study_cases import StudyCases
 from utils.array_file_manager import ArrayFileManager
 
-def main(data_path, occupancy):
-    # Defining the number of slices and slice size (TODO: change)
-    n_slices = 1_000_000
-    slice_size = 7
+def main(data_path, occupancy, slice_size):
+    if np.mod(slice_size, 2) != 0:
+        # Loading data
+        filename = f"training_occupancy_{occupancy}.csv"
+        training_dataset = ArrayFileManager.read_array_from_file(data_path, filename)
 
-    filename = f"training_occupancy_{occupancy}_slice_{slice_size}.csv"
-    training_dataset = ArrayFileManager.read_array_from_file(data_path, filename)
+        filename = f"test_occupancy_{occupancy}.csv"
+        test_dataset = ArrayFileManager.read_array_from_file(data_path, filename)
 
-    filename = f"test_occupancy_{occupancy}_slice_{slice_size}.csv"
-    test_dataset = ArrayFileManager.read_array_from_file(data_path, filename)
+        # Retrieving data size
+        data_size, _ = np.shape(training_dataset)
 
-    # Running results
-    analysis_cases = StudyCases(training_dataset, test_dataset, n_slices, slice_size, occupancy)
-    analysis_cases.run_case_1(save_file=True)
+        # Discarding edge data and calculating the number of slices
+        discard_size = np.mod(data_size, slice_size)
+        if discard_size == 0:
+            n_slices = data_size // slice_size
+        else:
+            n_slices = (data_size - discard_size) // slice_size
+            training_dataset = training_dataset[:-discard_size, :]
+            test_dataset = test_dataset[:-discard_size, :]
+
+        # Running analysis
+        analysis_cases = StudyCases(training_dataset, test_dataset, n_slices, slice_size, occupancy)
+        analysis_cases.run_case_1(save_file=True)
+    else:
+        raise ValueError("Slice size must be an odd number.")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--data_path", type=str, help="Calorimetry pulse data relative path (default: data/)")
     parser.add_argument("--occupancy", type=float, help="Occupancy (0.1 | 0.3 | 0.5)")
+    parser.add_argument("--slice_size", type=int, help="Slice size (integer greater than zero)")
     
     args = parser.parse_args()
-    if args.data_path and args.occupancy:
-        main(args.data_path, args.occupancy)
+    if args.data_path and args.occupancy and args.slice_size:
+        main(args.data_path, args.occupancy, args.slice_size)
     else:
         print("Please specify both the calorimetry pulse data relative path and occupancy.")
